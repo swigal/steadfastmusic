82926c7ba8c2d550b50a1521fb8bf5b0c043d52a
swigal <hello@samuelwigal.com>
2026-02-08 22:37:57 -0500

    Add Music resources

--- music-player.js
+++ music-player.js
@@ -10,22 +10,76 @@ const nextSongButton = document.getElementById("nextSong");
 
 const songs = [
     {
-        image: "./music/2026-selections/i-will-sing-praise.jpg",
+        image: "./music/2026-schedule/2026-schedule.jpeg",
         name: "I Will Sing Praise",
         artist: "arr. Dave Williamson",
-        audio: "./music/2026-selections/I_Will_Sing_Praise_-_Demo.mp3"
+        audio: "./music/2026-schedule/iI-Sing-Alleluia-LSTN.mp3"
     },
     {
-        image: "./music/2026-selections/a-blessing.jpg",
+        image: "./music/2026-schedule/2026-schedule.jpeg",
         name: "A Blessing",
         artist: "arr. Dennis Allen",
-        audio: "./music/2026-selections/A_Blessing_-_Listening_Demo.mp3"
+        audio: "./music/2026-schedule/A%20Blessing%20-%20Listening%20Demo.mp3"
     },
     {
-        image: "./music/2026-selections/search-me-lord.jpg",
+        image: "./music/2026-schedule/2026-schedule.jpeg",
+        name: "I Will Sing Praise",
+        artist: "arr. Dave Williamson",
+        audio: "./music/2026-schedule/I%20Will%20Sing%20Praise%20-%20Demo.mp3"
+    },
+    {
+        image: "./music/2026-schedule/2026-schedule.jpeg",
         name: "Search Me, Lord",
         artist: "arr. Jimm Hammerly",
-        audio: "./music/2026-selections/Search_Me_-_Demonstration.mp3",
+        audio: "./music/2026-schedule/Search%20Me%20-%20Demonstration.mp3",
+    },
+    {
+        image: "./music/2026-schedule/2026-schedule.jpeg",
+        name: "Hide Me",
+        artist: "arr. Dennis Allen",
+        audio: "./music/2026-schedule/Hide-Me.mp3",
+    },
+    {
+        image: "./music/2026-schedule/2026-schedule.jpeg",
+        name: "When You Speak to Me",
+        artist: "arr. Kyle Hill",
+        audio: "./music/2026-schedule/When%20You%20Speak%20-%20Demonstration.mp3",
+    },
+    {
+        image: "./music/2026-schedule/2026-schedule.jpeg",
+        name: "Blessed Is He",
+        artist: "arr. Dennis Allen",
+        audio: "./music/2026-schedule/Blessed%20is%20He%20-%20Listening%20Demo.mp3",
+    },
+    {
+        image: "./music/2026-schedule/2026-schedule.jpeg",
+        name: "This Is Our Freedom",
+        artist: "arr. Dennis Allen",
+        audio: "./music/2026-schedule/This%20Is%20Our%20Freedom%20-%20Demonstration.mp3",
+    },
+    {
+        image: "./music/2026-schedule/2026-schedule.jpeg",
+        name: "Amazing Love",
+        artist: "arr. Bob Burroughs",
+        audio: "./music/2026-schedule/Amazing%20Love%20-%20Demonstration.mp3",
+    },
+    {
+        image: "./music/2026-schedule/2026-schedule.jpeg",
+        name: "Hallelujah",
+        artist: "arr. Patrick",
+        audio: "./music/2026-schedule/Hallelujah%20-%20Patrick.mp3",
+    },
+    {
+        image: "./music/2026-schedule/2026-schedule.jpeg",
+        name: "Tis So Sweet to Trust in Jesus",
+        artist: "arr. Dennis Allen",
+        audio: "./music/2026-schedule/Tis%20So%20Sweet%20-%20Demonstration.mp3",
+    },
+    {
+        image: "./music/2026-schedule/2026-schedule.jpeg",
+        name: "Holy",
+        artist: "arr. John Patrick",
+        audio: "./music/2026-schedule/Holy%20-%20Demo.mp3",
     },
 ];
 
@@ -34,7 +88,7 @@ let currentSongIndex = 0;
 
 updateSong();
 
-prevSongButton.addEventListener("click", function() {
+/*prevSongButton.addEventListener("click", function() {
     if (currentSongIndex == 0) {
         return;
     }
@@ -48,6 +102,18 @@ nextSongButton.addEventListener("click", function() {
     }
     currentSongIndex++;
     updateSong();
+});*/
+
+playpauseButton.addEventListener("click", function(){
+    if (audio.paused) {
+    playpauseButton.classList.remove('fa-solid', 'fa-circle-play');
+    playpauseButton.classList.add('fa-regular', 'fa-circle-pause');
+    console.log("Icon changed to pause");
+  } else {
+    playpauseButton.classList.remove('fa-regular', 'fa-circle-pause');
+    playpauseButton.classList.add('fa-solid', 'fa-circle-play');
+    console.log("Icon changed to play");
+  }
 });
 
 playpauseButton.addEventListener("click", function() {
@@ -78,6 +144,14 @@ songSlider.addEventListener("change", function() {
 
 function moveSlider() {
     songSlider.value = audio.currentTime;
+};
+
+function changeSongIndex(index) {
+    currentSongIndex = index;
+    updateSong();
+    playpauseButton.classList.remove('fa-solid', 'fa-circle-play');
+    playpauseButton.classList.add('fa-regular', 'fa-circle-pause');
+    audio.play();
 };
 
 setInterval(moveSlider, 1000);
